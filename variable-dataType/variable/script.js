d2522eba55831a5b49a7efedf4628a519bfc4b9c
Muhammad Nurul Momen <momeniiuc7@gmail.com>
2024-06-05 15:50:00 +0600

    Create script.js

---
+++ variable-dataType/variable/script.js
@@ -0,0 +1,19 @@
+let Fname="john"
+let LName = "Doe"
+console.log(Fname,LName, typeof Fname)
+var age = 30;
+console.log(age);
+
+const age1 = 5;
+
+
+const arr = [1,2,3,4];
+arr.push(5);
+console.log(arr);
+
+const person = {
+    name: "Jagoar"
+};
+person.name = "Audi"
+
+console.log(person)
