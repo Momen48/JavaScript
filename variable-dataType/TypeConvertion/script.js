2fef6fa558fb6432450ed884806e3ce06f1aafd7
Muhammad Nurul Momen <momeniiuc7@gmail.com>
2024-06-09 18:59:03 +0600

    Create script.js

---
+++ variable-dataType/TypeConvertion/script.js
@@ -0,0 +1,22 @@
+
+// change String to number
+
+let amount = '100'; //counted as String
+
+//amount = parseInt(amount);
+//amount = + amount;             /* unary + operator */
+//amount = Number(amount);        /*number method or constructor */
+amount = parseFloat(amount);     /*String to Decimal*/
+
+console.log(amount, typeof amount);
+
+// Change Number to String 
+
+let num = 99.5;        //counted as Number
+//num = num.toString();     //primitive k non pritive di convert korai JS built in Temporart Wrapper create kore.
+
+
+
+
+console.log(num, typeof num);
+
