f2557389d93bc225b263b60ae39aebf071053c84
Muhammad Nurul Momen <momeniiuc7@gmail.com>
2024-08-17 01:54:24 +0600

    Update Script.js

--- Functions/1-Basic/Script.js
+++ Functions/1-Basic/Script.js
@@ -15,7 +15,28 @@ console.log(sum(1,2,3,4));
 
 //default   parameter
 
-function register(name='Bot'){
-    return name +  ` is registered`;
+// function register(name='Bot'){
+//     return name +  ` is registered`;
+// }
+// console.log(register('John'));
+
+// function userLogin(user){
+//     return `the user ${user.name} has logged in with user id: ${user.id} `;
+// }
+
+// const detail = {
+//     id: 1,
+//     name: 'Don'
+// }
+
+// console.log(userLogin(detail));
+
+// random number from an array
+
+function randomNum(...arr){
+    const index = Math.floor(Math.random()* arr.length);
+    const item= arr[index];
+    return console.log(item);
+    
 }
-console.log(register('John'));
+randomNum(1,2,3,4,5,6,7,8,9);
